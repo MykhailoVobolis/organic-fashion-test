2dd4ae607762bd5164c4a3dbd7a4ebdaefc52a8d
MykhailoVobolis <mvobolis@gmail.com>
2025-05-23 14:06:32 +0300

    fixed ResponsiveImage

--- src/components/ResponsiveImage/ResponsiveImage.jsx
+++ src/components/ResponsiveImage/ResponsiveImage.jsx
@@ -1,21 +1,17 @@
 export default function ResponsiveImage({ image, wrapperClasses = "" }) {
   return (
     <div className={wrapperClasses}>
-      <img
-        src={image.sources.desktop.x1}
-        srcSet={`
-    ${image.sources.mobile.x1} 375w,
-    ${image.sources.mobile.x2} 750w,
-    ${image.sources.tablet.x1} 768w,
-    ${image.sources.tablet.x2} 1536w,
-    ${image.sources.desktop.x1} 1440w,
-    ${image.sources.desktop.x2} 2880w
-  `}
-        sizes="(max-width: 768px) 375px, (max-width: 1024px) 768px, 1440px"
-        alt={image.alt}
-        className="w-full h-auto"
-        loading="lazy"
-      />
+      <picture>
+        <source media="(max-width: 768px)" srcSet={`${image.sources.mobile.x1} 1x, ${image.sources.mobile.x2} 2x`} />
+        <source media="(max-width: 1024px)" srcSet={`${image.sources.tablet.x1} 1x, ${image.sources.tablet.x2} 2x`} />
+        <img
+          src={image.sources.desktop.x1}
+          srcSet={`${image.sources.desktop.x1} 1x, ${image.sources.desktop.x2} 2x`}
+          alt={image.alt}
+          className="w-full h-auto"
+          loading="lazy"
+        />
+      </picture>
     </div>
   );
 }
